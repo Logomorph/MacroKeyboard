169b3f3d9469f157f8b244bfe3cf42890ef4fb20
logomorph <logomorph@gmail.com>
2019-11-02 14:04:07 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# MacroKeyboard
-Arduino-based, 3D printed, wireless mechanical macro keyboard
+# Macro Keyboard
+A 15-key Arduino-based, bluetooth wireless, mechanical macro keyboard.
+I made this to improve my workflow at work and to avoid the finger gymnastics characteristic to iOS development.
+# Glamour shots
+# How it works
+The keyboard is designed to work in conjunction with a tool like BetterTouchTool, which maps key presses to other shortcuts, based on the app in focus. 
+The keyboard transmits <kbd>CMD</kbd> + <kbd>control</kbd> + <kbd>shift</kbd> + <kbd>F1</kbd> -> <kbd>F12</kbd> and <kbd>1</kbd> ->  <kbd>3</kbd>, to cover all the keys.
+Using BetterTouchTool, these inputs can be mapped to various actions for each app.
+Besides this, the top left, when long pressed, transmits kbd>CMD</kbd> + <kbd>control</kbd> + <kbd>shift</kbd> + <kbd>option</kbd> + <kbd>F1</kbd> and puts the keyboard in sleep mode. This is useful for setting up a shortcut to lock the Mac.
+
+# Bill of materials
+| Item | Description |
+| ------ | ------ |
+| 15 CHerry MX Mechanical switches with diode | I used Ebay, but the closest/cheapest the better |
+| Arduino Pro Micro 32u4 (I used a clone) | Ebay |
+| HC-05 bluetooth module | Anywhere, but take care to not get the "fake" one. You need to flash it with the RN-42 Firmware and the "fake" one won't work. Google is your friend here |
+| Adafruit USB-C breakout | https://www.adafruit.com/product/4090 |
+| 3mm LED | Used to indicate various actions. I used blue to match my case |
+| 1400 MaH Lipo Battery JA-803450P | You can pick another one, but the case is designed with these dimensions in mind |
+| TP4056 LiPo charger | Ebay or Aliexpress |
+| Diodes | |
+| P-channel mosfet | Used for LiPo bypass when charging. We don't want to drain the battery while it's charging, as this can break the LiPo |
+| Resistors | Used as pull-down resistors for the keyboard matrix and as a voltage divider to check the LiPo battery level |
+| Keycaps | I 3D printed some from https://www.thingiverse.com/thing:468651, but you can pick whatever you fancy |
+
+# The circuit
+The keys are set up using a keyboard matrix. The top-left key is not included in the matrix, as it has to be able to wake the Arduino from sleep.
+
+# Missing stuff
+- Sending keys through the USB connection. Technically possible and the reason why I chose a 32u4 Arduino, but still to be implemented
+- LiPo voltage reading isn't the best
---
+++ src/macro_keyboard.ino
@@ -0,0 +1,292 @@
+#include <LowPower.h>
+#include <SoftwareSerial.h>
+#include <Keyboard.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+
+int r[3] = { 9, 8, 10 };
+int c[5] = { 6, 5, 4, 7, 2};
+int masterKey = 3;
+int statusLed = A2;
+int internalLed = 17;
+int btPower = A3;
+int lipoVoltage = A0;
+int sleepTriggerThreshold = 2000; // 2 seconds
+unsigned long sleepThreshold = 300000; // 5 minutes
+bool sentEmptyKey;
+unsigned long lastKeyPress;
+
+#define RX_PIN 16
+#define TX_PIN 14
+
+//F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 1 2 3
+byte btKeys[15] = { 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x1E, 0x1F, 0x20 };
+
+SoftwareSerial btSerial(RX_PIN, TX_PIN);
+
+struct KeyInfo {
+  unsigned long val;
+  unsigned long last;
+};
+KeyInfo matrix[15];
+
+// Battery management
+float lipoValue = 0.0;
+unsigned long lastBatteryCheck = 0;
+unsigned long lastBatteryWarning = 0;
+bool isLowBattery = false;
+bool isEmptyBattery = false;
+
+void setup() {
+  lastKeyPress = millis();
+  pinMode(lipoVoltage, INPUT);
+  pinMode(statusLed, OUTPUT);
+  digitalWrite(statusLed, LOW);
+  pinMode(btPower, OUTPUT);
+  digitalWrite(btPower, HIGH);
+  enableStatusLed(true);
+  resetKeyboardMatrix();
+  for (int i = 0; i < 3; i++) {
+    pinMode(r[i], OUTPUT);
+    digitalWrite(r[i], LOW);
+  }
+  for (int i = 0; i < 5; i++) {
+    pinMode(c[i], INPUT);
+    digitalWrite(c[i], LOW);
+  }
+  pinMode(masterKey, INPUT);
+  digitalWrite(masterKey, LOW);
+  
+  Serial.begin(115200);
+  enableBluetooth(true);
+  readKeyboardData();
+  processStartupHotkeys();
+}
+
+void loop() {
+  readKeyboardData();
+  processKeyPresses();
+  monitorBattery();
+  
+  while (btSerial.available() > 0) {
+    Serial.write(btSerial.read());
+  }
+  while (Serial.available() > 0) {
+    btSerial.write(Serial.read());
+  }
+  if (millis() - lastKeyPress > sleepThreshold) {
+    enterSleep();
+  }
+}
+
+void monitorBattery() {
+  unsigned long currentTime = millis();
+  // Only check the voltage every 10 seconds
+  if (currentTime - lastBatteryCheck > 10000 && !isLowBattery && !isEmptyBattery) {
+    return;
+  }
+  lastBatteryCheck = currentTime;
+  lipoValue = analogRead(lipoVoltage);
+  lipoValue = (lipoValue * 2.0f *  4.7f) / 1024.0f;
+  if (lipoValue < 3.6) {
+    isLowBattery = true;
+  }
+  if (lipoValue < 3.5) {
+    isEmptyBattery = true;
+  }
+  if (isEmptyBattery && currentTime - lastBatteryWarning > 1000) {
+    enableStatusLed(true);
+    lastBatteryWarning = currentTime;
+  } else if (isLowBattery && currentTime - lastBatteryWarning > 3000) {
+    enableStatusLed(true);
+    lastBatteryWarning = currentTime;
+  }
+}
+
+void resetKeyboardMatrix() {
+  for (int i = 0; i < 15; i++) {
+    matrix[i].val = 0;
+    matrix[i].last = 0;
+  }
+}
+
+void readKeyboardData() {
+  int val = digitalRead(masterKey);
+  processKey(0, 0, val);
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(r[i], HIGH);
+    for (int j = 0; j < 5; j++) {
+      if (i == 0 && j == 0) {
+        continue;
+      }
+      int val = digitalRead(c[j]);
+      processKey(i, j, val);
+    }
+    digitalWrite(r[i], LOW);
+  }
+  enableStatusLed(false);
+}
+
+void processKey(int i, int j, int val) {
+  int index = i*5 + j;
+  if (val == 1) {
+    if (matrix[index].val == 0 ) {
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.println(j);
+      matrix[index].val = millis();
+      enableStatusLed(true);
+    }
+  } else {
+    matrix[index].val = 0;
+  }
+}
+
+void processStartupHotkeys() {
+  enableStatusLed(true);
+  if (matrix[0].val != 0 && matrix[2].val != 0) {
+    btClearPairedDevices();
+  }
+  if (matrix[0].val != 0 && matrix[1].val != 0) {
+    btEnterPairMode();
+  }
+  enableStatusLed(false);
+}
+
+void processKeyPresses() {
+  unsigned long currentTime = millis();
+  if (currentTime - matrix[0].val > sleepTriggerThreshold && matrix[0].val != 0) {
+    if (matrix[1].val != 0) {
+      btSerial.print("Logokeys voltage: ");
+      btSerial.println(lipoValue);
+      delay(1000);
+      resetKeyboardMatrix();
+      return;
+    }
+    resetKeyboardMatrix();
+
+    byte data[] = {0xFD, 0x9, 0x1, (byte)0b00001111, (byte)0x0,(byte)0x3A,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0};
+    btSerial.write(data, sizeof(data));
+//    delay(50);
+//    byte data3[] = {0xFD, 0x3, 0x3, 0x08, (byte)0x0};
+//    btSerial.write(data3, sizeof(data3));
+//    delay(50);
+    byte data2[] = {0xFD, 0x9, 0x1, (byte)0x0, (byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0,(byte)0x0};
+    btSerial.write(data2, sizeof(data2));
+    
+    //btSerial.write(0x3A);
+    //Keyboard.write(0xf8);
+    enableStatusLed(true);
+    delay(1000);
+    enableStatusLed(false);
+    enterSleep();
+//    matrix[0].val = millis();
+  } else {
+    byte data[] = {0xFE, 0x5, (byte)0b0000111, (byte)0x0, (byte)0x0, (byte)0x0, (byte)0x0, (byte)0x0};
+    byte keysDown = 0;
+    int index = 3;
+    for (int i=0; i<15; i++) {
+      if (matrix[i].val != 0 && matrix[i].last == 0 && keysDown < 5) {
+        data[index] = btKeys[i];
+        keysDown++;
+        index++;
+        matrix[i].last = matrix[i].val;
+        lastKeyPress = currentTime;
+      }
+      if (matrix[i].val == 0 && matrix[i].last != 0) {
+        matrix[i].last = 0;
+      }
+    }
+    if (keysDown != 0) {
+      sentEmptyKey = false;
+    }
+    if (keysDown == 0 && !sentEmptyKey) {
+      sentEmptyKey = true;
+      byte empty[] = {0xFE, 0x0};
+      btSerial.write(empty, sizeof(empty));
+    }
+    if (keysDown == 0 && sentEmptyKey) {
+      return;
+    }
+    byte* finalArray = (byte*) malloc((keysDown+3) * sizeof(byte));
+    
+    finalArray[1] = keysDown;
+    for (byte i=0;i<keysDown+3;i++) {
+      if (i == 1) {
+        finalArray[i] = keysDown + 1;
+      } else {
+        finalArray[i] = data[i];
+      }
+      byte d = finalArray[i];
+      Serial.print(d, HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+    btSerial.write(finalArray, (keysDown+3) * sizeof(byte));
+    delete [] finalArray;
+    
+//    byte data[] = {0xFE, 0x2, (byte)0b0001000, (byte)0x16};
+//    btSerial.write(data, sizeof(data));
+//    delay(50);
+//    btSerial.write(0xFD);
+//    btSerial.write((byte)0x0);
+  }
+}
+
+void btSetBaudRate() {
+  digitalWrite(btPower, HIGH);
+  btSerial.begin(115200);
+  btSerial.print("CMD");
+  delay(100);
+  btSerial.println("U,9600,E");
+  delay(100);
+  btSerial.println("R1");
+}
+
+void btEnterPairMode() {
+  btSerial.print("CMD");
+  delay(100);
+  btSerial.println("SM,6");
+  delay(100);
+  btSerial.println("---");
+}
+
+void btClearPairedDevices() {
+  btSerial.print("CMD");
+  delay(100);
+  btSerial.println("SR,Z");
+  delay(100);
+  btSerial.println("---");
+}
+
+void enableBluetooth(bool enabled) {
+  if (enabled) {
+    digitalWrite(btPower, HIGH);
+    btSerial.begin(115200);
+  } else {
+    digitalWrite(btPower, LOW);
+    btSerial.end();
+  }
+}
+
+void enableStatusLed(bool enabled) {
+  digitalWrite(statusLed, enabled ? HIGH : LOW);
+}
+
+void enterSleep()
+{
+  attachInterrupt(digitalPinToInterrupt(3), wakeInterrupt, RISING);
+  enableBluetooth(false);
+  delay(100);
+  
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+
+
+  lastKeyPress = millis ();
+  Serial.begin(115200);
+  enableBluetooth(true);
+  enableStatusLed(true);
+  delay(200);
+}
+
+void wakeInterrupt(void) { }
